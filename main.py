c5768097e28db8b688aaa7b357a1220c0c4918d7
JosephPrause <Joeprau96@gmail.com>
2022-05-15 22:31:45 -0400

    v0.0.4 Part 3 Complete. Created Dynamic Dungeon Generation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from actions import EscapeAction, MovementAction
 from input_handlers import EventHandler
 from entity import Entity
 from engine import Engine
-from game_map import GameMap
+from procgen import generate_dungeon
 
 def main():
 	print("Loading God Debris Roguelike")
@@ -14,6 +14,10 @@ def main():
 
 	map_width = 80
 	map_height = 45
+
+	room_max_size = 10
+	room_min_size = 6
+	max_rooms = 30
 
 	##init tileset
 	tileset = tcod.tileset.load_tilesheet(
@@ -28,7 +32,7 @@ def main():
 	npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
 	entities = {npc, player}
 
-	game_map = GameMap(map_width, map_height)
+	game_map = generate_dungeon(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
 	engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
