6edbd79cef591885b3044075dd965e22977b38f3
JosephPrause <Joeprau96@gmail.com>
2022-05-15 20:08:27 -0400

    v0.0.2 - Add

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
 import tcod
 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
 def main():
-	print("Hello World!")
+	print("Loading God Debris Roguelike")
+	##Initialize Starting Variables
+	screen_width = 80
+	screen_height = 50
+	player_x = int(screen_width / 2)
+	player_y = int(screen_height / 2)
+
+	##init tileset
+	tileset = tcod.tileset.load_tilesheet(
+		"dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+	)
+
+	event_handler = EventHandler()
+
+	with tcod.context.new_terminal(
+		screen_width,
+		screen_height,
+		tileset = tileset,
+		title="God Debris Roguelike",
+		vsync=True
+
+	) as context:
+		root_console = tcod.Console(screen_width, screen_height, order="F")
+		while True:
+			root_console.print(x=player_x,y=player_y,string="@")
+
+			context.present(root_console)
+			root_console.clear()
+			
+			for event in tcod.event.wait():
+				action = event_handler.dispatch(event)
+
+				if action is None:
+					continue
+
+				if isinstance(action, MovementAction):
+					player_x+=action.dx
+					player_y+=action.dy
+
+				elif isinstance(action, EscapeAction):
+					raise SystemExit()
+
+	print("Exiting God Debris Roguelike")
+
 
 if __name__ == "__main__":
 	main()
